c8d2d77e9b2e8e65f0efbce93ee4fbb5099f4b63
John-Hoon Han <jh92aa@gmail.com>
2022-02-20 13:52:16 +0900

    act 4-1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,12 @@
 
+import Btn from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div className="App">
-      Hello World! 
+      <h1 className={ styles.title }>Hello World! </h1>
+      <Btn text="new btn!" />
     </div>
   );
 }
